243c721491294ef27ff4fa90f6d79f4e5f3b992f
gionni <giovbat@gmail.com>
2018-04-05 05:57:54 +0200

    updated composer.json + base TestTest

---
+++ tests/TestTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PHP version 7
+ * Base test class
+ */
+
+namespace Cineboard\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TestTest
+ *
+ * @package Cineboard\Tests
+ */
+class TestTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
