3364b49821be3ae12fe8bb0e1f9c35694786d68a
gionni <giovbat@gmail.com>
2018-04-06 01:10:11 +0200

    tests: base class for models

---
+++ tests/AbstractModelTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cineboard\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/**
+ * Class AbstractModelTest
+ *
+ * @package Cineboard\Tests
+ */
+abstract class AbstractModelTest extends TestCase
+{
+    /**
+     * @beforeClass
+     */
+    public static function prepareEloquentConnection()
+    {
+        $configFile = __DIR__ . '/../src/settings.local.php';
+        self::assertFileExists($configFile);
+
+        $settings = include $configFile;
+        self::assertInternalType('array', $settings);
+
+        self::assertArrayHasKey('settings', $settings);
+        self::assertArrayHasKey('database', $settings['settings']);
+
+        $capsule = new Capsule;
+        self::assertInstanceOf('Illuminate\Database\Capsule\Manager', $capsule);
+        $capsule->addConnection($settings['settings']['database']);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
+}
