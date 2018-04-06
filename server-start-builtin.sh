2d9b649a2ea431628ef50277e55bc4739760bb87
gionni <giovbat@gmail.com>
2018-04-06 08:58:51 +0200

    bash assets to start builtin php server

---
+++ server-start-builtin.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# quickly launch php builtin server - choose port
+
+if [ $# -eq 0 ]
+  then
+    echo -e "usage: bash $(basename "$0") 8081 or other port \n"
+    exit 1
+fi
+
+PORT=$1
+
+echo -e "Launching php builtin server on port "$PORT" \n"
+
+php -S 0.0.0.0:$PORT -t public public/index.php
+
