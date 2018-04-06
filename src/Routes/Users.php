4f54c98d52c1d539c3c2fc328bf382a7456169fb
gionni <giovbat@gmail.com>
2018-04-06 19:17:46 +0200

    users routes: refactoring with sanity checks

--- src/Routes/Users.php
+++ src/Routes/Users.php
@@ -4,70 +4,91 @@ use Cineboard\Model\User;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+/** @var \Slim\App $app */
 global $app;
 
-// Routes
-
-// read all records
 $app->get('/users', function (Request $request, Response $response, array $args) {
-    $data = User::all();
-    $resp["status"] = "OK";
-    $resp["data"] = $data;
-    return $response->withJson($resp);
+    unset($request);
+    $users = User::all();
+    if (!$users) {
+        $data = "record not found";
+        return $response->withJson($data, 404);
+    }
+    return $response->withJson($users, 200);
 });
 
 // return single record
-$app->get('/users/{id}', function (Request $request, Response $response, $args) {
+$app->get('/users/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
     unset($request);
-    $data = User::find($args["id"]);
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+    $user = User::find($args["id"]);
+    if (!$user) {
+        $data = "record not found";
+        return $response->withJson($data, 404);
     }
-    $resp["data"] = $data;
-    return $response->withJson($resp);
+    return $response->withJson($user, 200);
 });
 
-// save/create data for a new record
-$app->post('/users', function (Request $request, Response $response, $args) {
-    $name = $request->getParam("name");
-    $data = new User();
-    $data->name = $name;
-    $data->save();
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+$app->post('/users', function (Request $request, Response $response, array $args) use ($app) {
+    $parsedBody = $request->getParsedBody();
+    $name = $parsedBody['name'];
+
+    $app->getContainer()
+        ->get('logger')
+        ->debug('Route /users has been called with args ' . var_export($args, true));
+
+    // name db varchar 30 - if not 404 to avoid useless db interrogation
+    if (strlen($name) > 30) {
+        $data = "name too long: max 30 chars";
+        return $response->withJson($data, 401);
     }
-    $resp["data"] = $data;
-    return $response->withJson($resp);
+
+    // name is unique - check it
+    $user = User::where('name', $name)->first();
+    // check instanceof instead of obj
+    if ($user instanceof User && $user->name == $name) {
+        $app->getContainer()->get('logger')->info("ERROR: NAME ALREADY EXISTS EXISTENT /users" . var_export($name, true));
+        $data = "name already exists";
+        return $response->withJson($data, 401);
+    }
+    // create if not exists
+    $user = new User();
+    $user->name = $name;
+    $user->save();
+    $this->logger->info("NEW USER CREATED " . var_export($user['name'], true));
+
+    return $response->withJson($user, 200);
 });
 
-// save/update for existing record
-$app->put('/users/{id}', function (Request $request, Response $response, $args) {
-    $name = $request->getParam("name");
-    $data = User::find($args["id"]);
-    if ($data) {
-        $data->name = $name;
-        $data->save();
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+$app->put('/users/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
+    $parsedBody = $request->getParsedBody();
+    $name = $parsedBody['name'];
+
+    if (strlen($name) > 30) {
+        $data = "name too long: max 30 chars";
+        return $response->withJson($data, 401);
     }
-    $resp["data"] = $data;
-    return $response->withJson($resp);
+
+    $user = User::find($args["id"]);
+    if (!$user instanceof User) {
+        $data = "record not found";
+        return $response->withJson($data, 404);
+    }
+
+    $user->name = $name;
+    $user->save();
+    return $response->withJson($user, 200);
 });
 
 // delete existing record
-$app->delete('/users/{id}', function (Request $request, Response $response, $args) {
-    $data = User::find($args["id"]);
-    $data->delete();
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+$app->delete('/users/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
+    unset($request);
+
+    $user = User::find($args["id"]);
+    if (!$user instanceof User) {
+        $data = "record not found";
+        return $response->withJson($data, 404);
     }
-    $resp["data"] = $data;
-    return $response->withJson($resp);
+
+    $user->delete();
+    return $response->withJson($user, 200);
 });
