05106444d9f6ba5862d6fce7ac0acc142229e78c
gionni <giovbat@gmail.com>
2018-04-08 07:14:44 +0200

    libraries route refactoring

--- src/Routes/Libraries.php
+++ src/Routes/Libraries.php
@@ -4,83 +4,210 @@ use Cineboard\Model\Library;
 use \Slim\Http\Request;
 use \Slim\Http\Response;
 
+/** @var \Slim\App $app */
 global $app;
 
-// read all records
-$app->get('/libraries', function (Request $request, Response $response, $args) {
-    $data = Library::all();
-    // build response
-    $resp["status"] = "OK";
-    $resp["data"] = $data;
-    // Returning response
-    return $response->withJson($resp);
-});
-
-// return single record
-$app->get('/libraries/{id}', function (Request $request, Response $response, $args) {
+$app->get('/libraries', function (Request $request, Response $response, array $args) {
     unset($request);
-
-    $data = Library::find($args["id"]);
-    // build response
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+    $libraries = Library::with('categories')->get();
+    if (!$libraries) {
+        $data = "libraries not found";
+        return $response->withJson($data, 404);
     }
-    $resp["data"] = $data;
-    // Returning response
-    return $response->withJson($resp);
+    return $response->withJson($libraries, 200);
 });
 
-// save/create data for a new record
-$app->post('/libraries', function (Request $request, Response $response, $args) {
-    unset($args);
-
-    $name = $request->getParam("name");
-    $data = new Library();
-    $data->name = $name;
-    $data->save();
-    // build response
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+$app->get('/libraries/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
+    unset($request);
+    $library = Library::with('categories')->find($args["id"]);
+    if (!$library) {
+        $data = "library not found";
+        return $response->withJson($data, 404);
     }
-    $resp["data"] = $data;
-    // Returning response
-    return $response->withJson($resp);
+    return $response->withJson($library, 200);
+});
+
+$app->post('/libraries', function (Request $request, Response $response, array $args) {
+    $parsedBody = $request->getParsedBody();
+
+    // match number of db field
+    if (count($parsedBody) > 8) {
+        $data = "too many argumets";
+        return $response->withJson($data, 401);
+    }
+
+    $userId = $parsedBody['user_id'];
+    $title = $parsedBody['title'];
+    $director = $parsedBody['director'];
+    $rating = $parsedBody['rating'];
+    $viewed = $parsedBody['viewed'];
+    $url = $parsedBody['url'];
+    $tags = $parsedBody['tags'];
+    $notes = $parsedBody['notes'];
+
+    // check db fields limits and null/not null
+
+    if (!isset($userId)) {
+        $data = "user_id cannot be null";
+        return $response->withJson($data, 401);
+    }
+
+    if (!isset($title)) {
+        $data = "title cannot be null";
+        return $response->withJson($data, 401);
+    }
+
+    // library title db varchar 200
+    if (mb_strlen($title, 'UTF-8') > 200) {
+        $data = "title too long: max 200 chars";
+        return $response->withJson($data, 401);
+    }
+
+    // library director db varchar 50
+    if (isset($director)) {
+        if (mb_strlen($director, 'UTF-8') > 50) {
+            $data = "director too long: max 50 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // library url db varchar 300
+    if (isset($url)) {
+        if (mb_strlen($url, 'UTF-8') > 300) {
+            $data = "url too long: max 300 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // library tags db text - application limit
+    if (isset($tags)) {
+        if (mb_strlen($tags, 'UTF-8') > 500) {
+            $data = "tags too long: max 500 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // library notes db text - application limit
+    if (isset($notes)) {
+        if (mb_strlen($notes, 'UTF-8') > 500) {
+            $data = "notes too long: max 500 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // create if not exists
+    $library = new Library();
+    $library->user_id = $userId;
+    $library->title = $title;
+    $library->director = $director;
+    $library->rating = $rating;
+    $library->viewed = $viewed;
+    $library->url = $url;
+    $library->tags = $tags;
+    $library->notes = $notes;
+    $library->save();
+    //$this->logger->info("NEW LIBRARY CREATED " . var_export($library['title'], true));
+
+    return $response->withJson($library, 200);
 });
 
 // save/update for existing record
-$app->put('/libraries/{id}', function (Request $request, Response $response, $args) {
-    $name = $request->getParam("name");
-    $data = Library::find($args["id"]);
-    $data->name = $name;
-    $data->save();
-    // build response
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+$app->put('/libraries/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
+    $parsedBody = $request->getParsedBody();
+
+    // match number of db field
+    if (count($parsedBody) > 8) {
+        $data = "too much argumets";
+        return $response->withJson($data, 401);
     }
-    $resp["data"] = $data;
-    // Returning response
-    return $response->withJson($resp);
+
+    $userId = $parsedBody['user_id'];
+    $title = $parsedBody['title'];
+    $director = $parsedBody['director'];
+    $rating = $parsedBody['rating'];
+    $viewed = $parsedBody['viewed'];
+    $url = $parsedBody['url'];
+    $tags = $parsedBody['tags'];
+    $notes = $parsedBody['notes'];
+
+    // check db fields limits and null/not null
+
+    if (!isset($userId)) {
+        $data = "user_id cannot be null";
+        return $response->withJson($data, 401);
+    }
+
+    if (!isset($title)) {
+        $data = "title cannot be null";
+        return $response->withJson($data, 401);
+    }
+
+    // library title db varchar 200
+    if (mb_strlen($title, 'UTF-8') > 200) {
+        $data = "title too long: max 200 chars";
+        return $response->withJson($data, 401);
+    }
+
+    // library director db varchar 50
+    if (isset($director)) {
+        if (mb_strlen($director, 'UTF-8') > 50) {
+            $data = "director too long: max 50 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // library url db varchar 300
+    if (isset($url)) {
+        if (mb_strlen($url, 'UTF-8') > 300) {
+            $data = "url too long: max 300 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // library tags db text
+    if (isset($tags)) {
+        if (mb_strlen($tags, 'UTF-8') > 500) {
+            $data = "tags too long: max 500 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    // library notes db text
+    if (isset($notes)) {
+        if (mb_strlen($notes, 'UTF-8') > 500) {
+            $data = "notes too long: max 500 chars";
+            return $response->withJson($data, 401);
+        }
+    }
+
+    $library = Library::find($args["id"]);
+    if (!$library instanceof Library) {
+        $data = "library not found";
+        return $response->withJson($data, 401);
+    }
+    $library->user_id = $userId;
+    $library->title = $title;
+    $library->director = $director;
+    $library->rating = $rating;
+    $library->viewed = $viewed;
+    $library->url = $url;
+    $library->tags = $tags;
+    $library->notes = $notes;
+    $library->save();
+
+    return $response->withJson($library, 200);
 });
 
 // delete existing record
-$app->delete('/libraries/{id}', function (Request $request, Response $response, $args) {
+$app->delete('/libraries/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
     unset($request);
 
-    $data = Library::find($args["id"]);
-    $data->delete();
-    // build response
-    if ($data) {
-        $resp["status"] = "OK";
-    } else {
-        $resp["status"] = "ERR";
+    $library = Library::find($args["id"]);
+    if (!$library instanceof Library) {
+        $data = "library not found";
+        return $response->withJson($data, 404);
     }
-    $resp["data"] = $data;
-    // Returning response
-    return $response->withJson($resp);
+    $library->delete();
+
+    return $response->withJson($library, 200);
 });
