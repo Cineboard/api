0947e0a2a1805a2aca9ca47bb88c4b5121b629f2
gionni <giovbat@gmail.com>
2018-04-08 07:16:38 +0200

    users route refactoring2: the return

--- src/Routes/Users.php
+++ src/Routes/Users.php
@@ -11,7 +11,7 @@ $app->get('/users', function (Request $request, Response $response, array $args)
     unset($request);
     $users = User::all();
     if (!$users) {
-        $data = "record not found";
+        $data = "users not found";
         return $response->withJson($data, 404);
     }
     return $response->withJson($users, 200);
@@ -22,7 +22,7 @@ $app->get('/users/{id:[0-9]+}', function (Request $request, Response $response, 
     unset($request);
     $user = User::find($args["id"]);
     if (!$user) {
-        $data = "record not found";
+        $data = "user not found";
         return $response->withJson($data, 404);
     }
     return $response->withJson($user, 200);
@@ -30,11 +30,24 @@ $app->get('/users/{id:[0-9]+}', function (Request $request, Response $response, 
 
 $app->post('/users', function (Request $request, Response $response, array $args) use ($app) {
     $parsedBody = $request->getParsedBody();
+
+    // match number of db field
+    if (count($parsedBody) > 1) {
+        $data = "too much argumets";
+        return $response->withJson($data, 401);
+    }
+
     $name = $parsedBody['name'];
 
     $app->getContainer()
         ->get('logger')
         ->debug('Route /users has been called with args ' . var_export($args, true));
+
+
+    if (!isset($name)) {
+        $data = "name cannot be null";
+        return $response->withJson($data, 401);
+    }
 
     // name db varchar 30 - if not 404 to avoid useless db interrogation
     if (mb_strlen($name, 'UTF-8') > 30) {
@@ -46,9 +59,6 @@ $app->post('/users', function (Request $request, Response $response, array $args
     $user = User::where('name', $name)->first();
     // check instanceof instead of obj
     if ($user instanceof User && $user->name == $name) {
-        $app->getContainer()
-        ->get('logger')
-        ->info("ERROR: NAME ALREADY EXISTS EXISTENT /users" . var_export($name, true));
         $data = "name already exists";
         return $response->withJson($data, 401);
     }
@@ -65,7 +75,17 @@ $app->post('/users', function (Request $request, Response $response, array $args
 
 $app->put('/users/{id:[0-9]+}', function (Request $request, Response $response, array $args) {
     $parsedBody = $request->getParsedBody();
+    if (count($parsedBody) > 1) {
+        $data = "too much argumets";
+        return $response->withJson($data, 401);
+    }
+
     $name = $parsedBody['name'];
+
+    if (!isset($name)) {
+        $data = "name cannot be null";
+        return $response->withJson($data, 401);
+    }
 
     if (mb_strlen($name, 'UTF-8') > 30) {
         $data = "name too long: max 30 chars";
@@ -74,7 +94,7 @@ $app->put('/users/{id:[0-9]+}', function (Request $request, Response $response, 
 
     $user = User::find($args["id"]);
     if (!$user instanceof User) {
-        $data = "record not found";
+        $data = "user not found";
         return $response->withJson($data, 404);
     }
 
@@ -89,7 +109,7 @@ $app->delete('/users/{id:[0-9]+}', function (Request $request, Response $respons
 
     $user = User::find($args["id"]);
     if (!$user instanceof User) {
-        $data = "record not found";
+        $data = "user not found";
         return $response->withJson($data, 404);
     }
 
