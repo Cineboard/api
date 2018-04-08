d7b75c1017209939cd84e6e48d7b1f362bc9ed09
gionni <giovbat@gmail.com>
2018-04-08 16:56:49 +0200

    createsql for <=5.6 - support to docker

---
+++ sql/create56.sql
@@ -0,0 +1,8 @@
+-- if mysql <= 5.6
+DROP DATABASE IF EXISTS cineboard_db ;
+CREATE DATABASE cineboard_db ;
+GRANT USAGE ON *.* TO 'cineboard_user'@'localhost' ;
+DROP USER 'cineboard_user'@'localhost' ;
+CREATE USER 'cineboard_user'@'localhost' IDENTIFIED BY 'cineboard_password' ;
+GRANT ALL PRIVILEGES ON cineboard_db.* TO 'cineboard_user'@'localhost' ;
+FLUSH PRIVILEGES ;
